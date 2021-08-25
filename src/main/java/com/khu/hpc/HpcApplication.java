072ac79ef24c2852dff4843666322c971b340d8f
f.tork <f.tork@yaas.bm>
2021-08-25 14:28:47 +0430

    intial commit - add dependencies - create spring boot project

---
+++ src/main/java/com/khu/hpc/HpcApplication.java
@@ -0,0 +1,13 @@
+package com.khu.hpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HpcApplication.class, args);
+    }
+
+}
